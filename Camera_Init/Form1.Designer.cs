21465d822d71418a52fd0696eb413d90e4566802
PuunCity <Bryan.Minciuna@gmail.com>
2022-05-08 21:11:53 -0400

    Color recognition Algorithm 1.0

--- Camera_Init/Form1.Designer.cs
+++ Camera_Init/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Camera_Init
 {
     partial class Form1
@@ -32,12 +34,15 @@ namespace Camera_Init
             this.button1 = new System.Windows.Forms.Button();
             this.CameraFeedBack = new System.Windows.Forms.PictureBox();
             this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.FrameCapture = new System.Windows.Forms.Button();
+            this.labelColorResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.CameraFeedBack)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(687, 23);
+            this.button1.Location = new System.Drawing.Point(23, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -47,9 +52,13 @@ namespace Camera_Init
             // 
             // CameraFeedBack
             // 
+            this.CameraFeedBack.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.CameraFeedBack.Location = new System.Drawing.Point(12, 67);
+            this.CameraFeedBack.MinimumSize = new System.Drawing.Size(600, 800);
             this.CameraFeedBack.Name = "CameraFeedBack";
-            this.CameraFeedBack.Size = new System.Drawing.Size(649, 371);
+            this.CameraFeedBack.Size = new System.Drawing.Size(649, 800);
             this.CameraFeedBack.TabIndex = 1;
             this.CameraFeedBack.TabStop = false;
             this.CameraFeedBack.Click += new System.EventHandler(this.CameraFeedBack_Click);
@@ -62,11 +71,41 @@ namespace Camera_Init
             this.cboCamera.Size = new System.Drawing.Size(273, 21);
             this.cboCamera.TabIndex = 2;
             // 
+            // FrameCapture
+            // 
+            this.FrameCapture.Location = new System.Drawing.Point(473, 12);
+            this.FrameCapture.Name = "FrameCapture";
+            this.FrameCapture.Size = new System.Drawing.Size(106, 23);
+            this.FrameCapture.TabIndex = 3;
+            this.FrameCapture.Text = "Frame Capture";
+            this.FrameCapture.UseVisualStyleBackColor = true;
+            this.FrameCapture.Click += new System.EventHandler(this.FrameCapture_Click);
+            // 
+            // labelColorResult
+            // 
+            this.labelColorResult.AutoSize = true;
+            this.labelColorResult.Location = new System.Drawing.Point(470, 38);
+            this.labelColorResult.Name = "labelColorResult";
+            this.labelColorResult.Size = new System.Drawing.Size(32, 13);
+            this.labelColorResult.TabIndex = 4;
+            this.labelColorResult.Text = "result";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(657, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelColorResult);
+            this.Controls.Add(this.FrameCapture);
             this.Controls.Add(this.cboCamera);
             this.Controls.Add(this.CameraFeedBack);
             this.Controls.Add(this.button1);
@@ -76,7 +115,13 @@ namespace Camera_Init
             this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.CameraFeedBack)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
+        }
+
+        private void CameraFeedBack_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
@@ -84,6 +129,9 @@ namespace Camera_Init
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox CameraFeedBack;
         private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.Button FrameCapture;
+        private System.Windows.Forms.Label labelColorResult;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Camera_Init/Form1.cs
+++ Camera_Init/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿// Touts les namespaces utilisés pour ce code.
+// Un namespace est une boîte à outil dans lequel tout le code nécessaire sera retrouvé.
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -10,42 +12,46 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
 
 
-
+// Le nom du namespace privé qui a été créé.
 namespace Camera_Init
 {
+    // Classe d'objet partielle déclarée sous type de Form
+    // Un Form est tout ce qui est élément graphique du code (boutons, retour vidéo, fenêtre, etc.)
     public partial class Form1 : Form
     {
-        
 
+        // Initialisation du Form pour le créer lors du lancement du code.
         public Form1()
         {
+            // méthode pour initialiser l'application
             InitializeComponent();
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
-        {
-            
-        }
+        // Création d'objets variables de deux types:
 
-        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-            CameraFeedBack.Image = (Bitmap)eventArgs.Frame.Clone();
-        }
-
+        //FilterInfoColletion: "variable de type informations sur la collection du filtre" le filtre qui va chercher pour trouver l'appareil de capture vidéo.
+        //VideoCaptureDevice: "variable de type Appareil de capture vidéo" la caméra utilisée pour l'execution du code.
         FilterInfoCollection filterInfoCollection;
         VideoCaptureDevice videoCaptureDevice;
 
 
-        private void CameraFeedBack_Click(object sender, EventArgs e)
+        // défintion d'une méthode privée sans valeur retourner et qui prend comme argument un objet qui va être utilisé comme messager et une variable de type NouvelleFrameParEvenement nommé eventArgs (le détecteur d'un évenement)
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
-
+            // On accede à la variable Image dans la classe de CameraFeedBack
+            CameraFeedBack.Image = (Bitmap)eventArgs.Frame.Clone();
         }
+
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
+            // Initialisation du filtre de la collecte d'informations
             filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
             foreach (FilterInfo filterInfo in filterInfoCollection)
             {
                 cboCamera.Items.Add(filterInfo.Name);
@@ -54,13 +60,15 @@ namespace Camera_Init
             videoCaptureDevice = new VideoCaptureDevice();
         }
 
+
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if(videoCaptureDevice.IsRunning == true)
+            if (videoCaptureDevice.IsRunning == true)
             {
                 videoCaptureDevice.Stop();
             }
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -68,5 +76,117 @@ namespace Camera_Init
             videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
             videoCaptureDevice.Start();
         }
+
+        private void FrameCapture_Click(object sender, EventArgs e)
+        {
+            Bitmap resim = new Bitmap(CameraFeedBack.Image);
+
+            EuclideanColorFiltering filter = new EuclideanColorFiltering();
+            // set center color and radius
+            filter.CenterColor = new RGB(245, 245, 220);
+            filter.Radius = 250;
+            // apply the filter
+            
+            //filter.ApplyInPlace(resim);
+
+            CameraFeedBack.Image = resim;
+            Color x = resim.GetPixel(300, 300);
+
+            videoCaptureDevice.Stop();
+            GetMostUsedColor(resim);
+
+            labelColor();
+            ColorDetection();
+
+
+
+        }
+
+
+        
+
+
+        public static List<Color> TenMostUsedColors { get; private set; }
+        public static List<int> TenMostUsedColorIncidences { get; private set; }
+
+        public static Color MostUsedColor { get; private set; }
+        public static int MostUsedColorIncidence { get; private set; }
+
+        private static int pixelColor;
+
+        private static Dictionary<int, int> dctColorIncidence;
+
+        public static void GetMostUsedColor(Bitmap theBitMap)
+        {
+            TenMostUsedColors = new List<Color>();
+            TenMostUsedColorIncidences = new List<int>();
+
+            MostUsedColor = Color.Empty;
+            MostUsedColorIncidence = 0;
+
+            // does using Dictionary<int,int> here
+            // really pay-off compared to using
+            // Dictionary<Color, int> ?
+
+            // would using a SortedDictionary be much slower, or ?
+
+            dctColorIncidence = new Dictionary<int, int>();
+
+            // this is what you want to speed up with unmanaged code
+            for (int row = 0; row < theBitMap.Size.Width; row++)
+            {
+                for (int col = 0; col < theBitMap.Size.Height; col++)
+                {
+                    pixelColor = theBitMap.GetPixel(row, col).ToArgb();
+
+                    if (dctColorIncidence.Keys.Contains(pixelColor))
+                    {
+                        dctColorIncidence[pixelColor]++;
+                    }
+                    else
+                    {
+                        dctColorIncidence.Add(pixelColor, 1);
+                    }
+                }
+            }
+
+            // note that there are those who argue that a
+            // .NET Generic Dictionary is never guaranteed
+            // to be sorted by methods like this
+            var dctSortedByValueHighToLow = dctColorIncidence.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            // this should be replaced with some elegant Linq ?
+            foreach (KeyValuePair<int, int> kvp in dctSortedByValueHighToLow.Take(10))
+            {
+                TenMostUsedColors.Add(Color.FromArgb(kvp.Key));
+                TenMostUsedColorIncidences.Add(kvp.Value);
+            }
+
+            MostUsedColor = Color.FromArgb(dctSortedByValueHighToLow.First().Key);
+            MostUsedColorIncidence = dctSortedByValueHighToLow.First().Value;
+        }
+
+        
+
+        private void labelColor()
+        {
+            labelColorResult.Text = MostUsedColor.ToString();
+        }
+
+        Color black = Color.Black;
+
+        private void ColorDetection()
+        {
+            if(MostUsedColor == black)
+            {
+                label1.Text = "BLACK DETECTED";
+            }
+            else
+            {
+                label1.Text = "BLACK NOT DETECTED";
+            }
+        }
+        
     }
+
 }
